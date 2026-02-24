21278bd553e620483d5919f7d391501e32593803
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:02:53 +0300

    Update what_pulse.py

--- src/what_pulse.py
+++ src/what_pulse.py
@@ -1,28 +1,25 @@
-from requests import get
+from requests import Session
 
 class WhatPulse:
 	def __init__(self) -> None:
 		self.api = "https://api.whatpulse.org"
-		self.headers = {
+		self.session = Session()
+		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def get_team_stats(self, team_name: str) -> dict:
-		return get(
-			f"{self.api}/team.php?team={team_name}&format=json",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/team.php?team={team_name}&format=json").json()
 
 	def get_team_pulse_stats(self, team_name: str) -> dict:
-		return get(
-			f"{self.api}/pulses.php?team={team_name}&format=json",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pulses.php?team={team_name}&format=json").json()
 
 	def get_team_pulse_stats(self, user_id: str) -> dict:
-		return get(
-			f"{self.api}/pulses.php?user={user_id}&format=json",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/pulses.php?user={user_id}&format=json").json()
 
 	def get_user_stats(self, user_id: int) -> dict:
-		return get(
-			f"{self.api}/user.php?user={user_id}&format=json",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/user.php?user={user_id}&format=json").json()
